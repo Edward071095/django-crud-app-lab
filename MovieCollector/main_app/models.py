67dcb3c0b1d5bfd473fbb548cd2c9fe73f377a6d
EdwardW710 <EdwardW710@yahoo.com>
2025-04-02 17:57:24 -0400

    all CRUD complete

--- MovieCollector/main_app/models.py
+++ MovieCollector/main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    released = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.title} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('movie-detail', kwargs={'movie_id': self.id})
